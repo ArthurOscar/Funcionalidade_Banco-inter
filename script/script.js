0d42d9e420a1fd889dde92d7b79ce0c48fd60119
ArthurOscar <arthur_o_soares@estudante.sesisenai.org.br>
2025-05-02 10:12:25 -0300

    UPDATE - Conversor em andamento

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 function mudarPagina(){
-    window.location.href = '../public/index_funcionalidade.html';
+    window.location.href = '../public/index_funcionalidade.html'
+}
+
+function voltarPagina(){
+    window.location.href = '../public/index.html'
 }
